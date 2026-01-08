63bcd88c36848cd9b15fd27c0fb6c68a23cb71ef
Mário Balça <mario.balca@gmail.com>
2026-01-08 12:16:40 +0000

    Yet one more attempt to fix proxy.ts

--- proxy.ts
+++ proxy.ts
@@ -3,7 +3,9 @@ import { NextResponse, type NextRequest } from 'next/server'
 
 export async function proxy(request: NextRequest) {
   let supabaseResponse = NextResponse.next({
-    request,
+    request: {
+      headers: request.headers,
+    },
   })
 
   const supabase = createServerClient(
@@ -19,7 +21,9 @@ export async function proxy(request: NextRequest) {
             request.cookies.set(name, value)
           );
           supabaseResponse = NextResponse.next({
-            request,
+            request: {
+              headers: request.headers,
+            },
           });
           cookiesToSet.forEach(({ name, value, options }) =>
             supabaseResponse.cookies.set({
